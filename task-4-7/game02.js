5de35f3c30cd7be8038abe4853040a80d5a9a2b8
Evgeny-Samoshin <samevgeny@gmail.com>
2024-06-23 21:44:05 +0400

    add task-4-7

--- task-4-7/game02.js
+++ task-4-7/game02.js
@@ -1,38 +1,116 @@
-const randomInt = Math.round(Math.random() * (100 - 1 + 1) + 1);
-
-const game = () => {
-  let usersInt = +prompt('Угадай число, испытуемый:');
-
-  console.log(usersInt);
-  if (Number.isNaN(usersInt)) {
-    alert('Введи число!');
-
-    game();
+const getArrayLength = (n, m) => {
+  if ((n < 0 && m < 0) || (n > 0 && m > 0)) {
+    return Math.abs(n - m);
   };
 
-  if (usersInt === 0) {
+  if (n < 0 || m < 0) {
+    return Math.abs(n) + Math.abs(m)
+  };
+};
+
+const generateArray = (n, m) => {
+  const arrayLength = getArrayLength(n, m);
+  const arr = [];
+
+  let i = 0;
+
+  if (n < m) {
+    while (i <= arrayLength) {
+      i++;
+      arr.push(n);
+      n += 1;
+    };
+  };
+
+  if (n > m) {
+    while (i <= arrayLength) {
+      i++;
+      arr.push(m);
+      m += 1;
+    }
+  };
+
+  if (n === m) {
+    arr.push(n);
+  };
+
+  return arr;
+};
+
+const getRandomInt = (n, m) => {
+  return Math.round(n <= m ? (Math.random() * (n - m) + m) : (Math.random() * (m - n) + n));
+};
+
+const getNumberOfAttempts = (arr) => {
+  return arr.join('').length * 0.3;
+};
+
+const checkAttempt = (userNum, pcNum, arr) => {
+
+  if (userNum === false) {
     alert('Испытание отменено.');
 
     return;
   };
 
-  if (usersInt < randomInt) {
-    alert('Меньше!');
-    console.log(randomInt, usersInt);
+  userNum = +userNum;
+
+  if (Number.isNaN(userNum)) {
+    alert('Введи число!');
+
     game();
-  };
-  
-  if (usersInt > randomInt) {
-    alert('Больше!');
-    console.log(randomInt, usersInt);
-    game();
-  };
-  
-  if (usersInt === randomInt) {
-    console.log(randomInt, usersInt);
-    alert('Правильно!');
   };
 
+  if (userNum < pcNum) {
+    alert('Меньше!');
+    console.log(userNum, pcNum);
+
+    arr.push(userNum);
+    game();
+  };
+
+  if (userNum > pcNum) {
+    alert('Больше!');
+    console.log(userNum, pcNum);
+
+    arr.push(userNum);
+    game();
+  };
+
+  if(arr.includes(userNum)) {
+    alert('Это число вы уже вводили.');
+
+    game();
+  }
+
+  if (+userNum === pcNum) {
+    console.log(userNum, pcNum);
+
+    alert('Испытание пройдено! Больше огня под ногами твоих врагов, испытуемый!');
+  };
+}
+
+const game = () => {
+  const firstNum = +prompt('Введи первое число, испытуемый:');
+  const secondNum = +prompt('Введи второе число, испытуемый:');
+  const failedAttempts = [];
+  let counter = 0; 
+
+  const rangeArr = generateArray(firstNum, secondNum);
+
+  const numberOfAttempts = getNumberOfAttempts(rangeArr);
+
+  const randomInt = getRandomInt(firstNum, secondNum);
+
+  const usersInt = prompt('Угадай число в заданном диапазоне, испытуемый:');
+  
+  counter++;
+  
+  checkAttempt(usersInt, randomInt, failedAttempts);
+
+  if (counter === numberOfAttempts) {
+    return;
+  }
 }
 
 game();
